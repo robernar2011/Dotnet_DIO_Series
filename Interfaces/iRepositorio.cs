362dd3e12e0b68353b0dad53f540860214f309cd
Your Name <robernar2011@gmail.com>
2021-05-21 16:13:07 -0300

    Primeiro commit

---
+++ Interfaces/iRepositorio.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace DIO.Series.Interfaces
+{
+    public interface iRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Exlui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();             
+    }
+}
